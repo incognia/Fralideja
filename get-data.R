b10461b5d9c29c557706d44f432e1c87260f0444
incognia <incognia@gmail.com>
2018-04-10 23:00:16 -0500

    Añadir "get-data.R" al repositorio.

---
+++ get-data.R
@@ -0,0 +1,53 @@
+#!/usr/bin/Rscript
+#   get-data.R - Twitter Data Mining App
+#
+#   Copyright © 2018, Rodrigo Ernesto Alvarez Aguilera <incognia@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#   Version 0.1 - April 4, 2018
+#   Author: Rodrigo Ernesto Alvarez Aguilera
+#   Author: Bas Hofstra
+#
+#   Tested under Ubuntu 16.04.4 LTS using R version 3.4.4
+#
+
+# Choose your working directory
+setwd("~/twitter/")
+
+# Load libraries
+library(twitteR)
+library(foreign)
+library(devtools)
+
+# The Consumer Key (API Key), Consumer Secret (API Secret),
+# Access Token and Access Token Secret needed to access the Twitter-API
+con <- "HLFmYtlDMSOQJaJVteg19t6RR"
+cons <-"p6j5gFjk5sKZDmFoJRWz7KXWGPEnkbTv75Ay5Gv7YJ2iXBKoKv"
+api <- "35599268-xKFomjh0K2Y6Uj5J3lVdkOHCoIecqVca4mrKn4IFi"
+apis <- "Rvj8q5jYQW0pzspYIB3bApOzruJXlCr4Xo2zdk3kgTErG"
+
+setup_twitter_oauth(con, cons, api, apis)
+
+# Format date and time
+date <- format(Sys.time(), "%y%m%d-%H%M%S")
+
+# So we want to download information surrounding "rebsamen"
+dataframe <- searchTwitter('rebsamen', n = 10000, retryOnRateLimit = 10000)
+dataframe <- do.call("rbind", lapply(dataframe, as.data.frame))
+dataframe$text <- gsub("\n", " ", dataframe$text) # Here some lines to delete linebreaks
+dataframe$text <- gsub("\r", " ", dataframe$text) # so that statistical software reads it
+dataframe$location <- gsub("\n", " ", dataframe$location) # nicely.
+dataframe$location <- gsub("\r", " ", dataframe$location)
+write.table(dataframe, paste0(date, "-rebsamen_data.txt"), sep = "\t", row.names = FALSE)
